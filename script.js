f735b873a19fee6c22b98ab4d42cf865dd86f3fa
Hennie Scheepers <hennie.104@hotmail.com>
2023-03-30 11:23:01 +0200

    Completed challenge IWA-4

--- script.js
+++ script.js
@@ -1,33 +1,44 @@
-const date: 2050
-const status: 'student'
-const count = 0
+//Some variables did not have an assignment operator
+const date = 2050
+let status = 'student'
+let count = 0
 
-if (date = 2050) {
-	console.log("January", 'New Year’s Day'
+if (date === 2050) {
+    //changed count to let as it will have a changing value
+    
+    //Missing bracket
+	console.log("January", 'New Year’s Day')
 	console.log("March", 'Human Rights Day')
-	date = 'April'
-	console.log(date, 'Family Day')
-	console.log(date, 'Freedom Day')
-	let count = count + 4
+    //created new 'month' variable as date has conflicting uses with global scope
+	let month = 'April'
+	console.log(month, 'Family Day')
+	console.log(month, 'Freedom Day')
+    //removed let as it has already been defined
+	count = count + 4
 
-	if (status = "student") {
+	if (status === "student") {
 	  console.log('June', 'Youth Day')
-		let count = count + 1
+      //removed let as it has already been defined
+	    count = count + 1
   }
 
 	console.log('August', 'Women’s Day')
 	console.log('September', 'Heritage Day')
-	date = 'December'
-	console.log(date, 'Day of Reconciliation')
-	let count = count + 3
+    //changed date to month
+	month = 'December'
+	console.log(month, 'Day of Reconciliation')
+    //removed let as it has already been defined
+	count = count + 3
 
-	if (status = "parent") {
-	  console.log(date, 'Christmas Day')
-		let count = count + 1
+	if (status === "parent") {
+        //changed date to month
+	  console.log(month, 'Christmas Day')
+      //removed let, already declared
+	    count = count + 1
   }
-
-	console.log(date, 'Day of Goodwill')
-	let count = count + 1
+    //changed date to month
+	console.log(month, 'Day of Goodwill')
+	count = count + 1
 }
 
 console.log('Your status is:', status)
